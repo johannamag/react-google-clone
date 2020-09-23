0d5fd10d9730f7e1874936dd431b58e7c40981e1
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-23 09:22:39 +0200

    Added Folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/*Homepage with search field*/}
+
+      <Home />
+
+      {/*Search Results Page*/}
+    </div>
+  );
 }
 
 export default App;
