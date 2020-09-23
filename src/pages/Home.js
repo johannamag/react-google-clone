12c2480047d39e6043e0b0bd8a120375b54fb589
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-23 09:33:26 +0200

    Installed Material UI and React Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,10 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <div className="home__header"></div>
+      <div className="home__header">
+        <div className="home__headerLeft"></div>
+        <div className="home__headerRight"></div>
+      </div>
       <div className="home__body"></div>
     </div>
   );
